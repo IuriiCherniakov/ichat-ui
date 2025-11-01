04e4a6a53c7c1ec5d827d9c6cff1a4effeb14735
Yury <chernyakov.yuriy@gmail.com>
2025-11-01 14:47:05 -0400

    Add useLogin hook for handling user authentication flow
    Signed-off-by: Yury <chernyakov.yuriy@gmail.com>

---
+++ src/hooks/useLogin.ts
@@ -0,0 +1,33 @@
+import {useState} from "react";
+import client from "../constants/apollo-client";
+import {API_URL} from "../constants/urls";
+
+interface LoginRequest {
+	email: string;
+	password: string;
+}
+
+const useLogin = () => {
+	const [error, setError] = useState<boolean>();
+
+	const login = async (request: LoginRequest) => {
+		const res = await fetch(`${API_URL}/auth/login`, {
+			method: "POST",
+			headers: {
+				"Content-Type": "application/json",
+			},
+			body: JSON.stringify(request),
+		});
+		if (!res.ok) {
+			setError(true);
+		}
+		setError(false);
+
+		// refetch cached queries
+		await client.refetchQueries({include: "active"});
+	};
+
+	return {login, error};
+};
+
+export {useLogin};
